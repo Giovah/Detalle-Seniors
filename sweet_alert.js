f1357c939d2272cd105fff8c2fb209aea30924a7
Giovah <72900950+Giovah@users.noreply.github.com>
2020-10-15 04:45:54 +0200

    Add files via upload

---
+++ sweet_alert.js
@@ -0,0 +1,28 @@
+Swal.mixin({
+	
+	backdrop: 'rgba(0,0,123,0.4)',
+	background: 'mediumorchid',
+	confirmButtonColor: '#C71585',
+	confirmButtonText: 'Ok &rarr;',
+	
+	
+}).queue([
+	{
+		title: '<strong><font color="white">Buen dia</font></strong>',
+		html: '<font color="white">Querido desarrollador Front End</font>'
+	},
+	'<font color="white">Talvez muchas veces te has sentido que ya no puedes seguir, los problemas te han afectado...</font>',
+
+
+	'<font color="white">Los problemas nunca se acaban pero... ¡las soluciones tampoco!</font>',
+
+
+	'<font color="white">Y eso es lo que has demostrado siendo un programador supérate</font>', 
+	
+	'<font color="white">Han recorrido un largo camino para llegar hasta aquí</font>',
+
+	'<font color="white">Se han esforzado duramente durante estos meses, para poder llegar al final</font>',
+
+	'<font color="white">Hoy es el día en que todos los entrenamientos y prácticas rendirán fruto.</font>'
+
+	])
